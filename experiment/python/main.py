82dde3d80f312defa8fd02b4f002cf3624e43888
yomi4486 <dovahkiin4486@icloud.com>
2024-11-02 22:00:54 +0900

    argfuhaiugarg

--- experiment/python/main.py
+++ experiment/python/main.py
@@ -1,13 +1,1 @@
-a,b="",""
-jp_result = """
-hello
-i'm yomi4486
-good bye
-"""
-text_list = jp_result.splitlines(keepends=True)
-for l in text_list:
-    if not len(a+l) >= 2000:
-        a+=l
-    else:
-        b+=l
-    print(a)
+print(__spec__)
